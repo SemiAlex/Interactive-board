67af42a74966d16042f29aa361a453f0a9ff1ab1
Alex <herbalpie@gmail.com>
2022-09-18 17:34:06 +0300

    123

---
+++ src/components/TaskTitle.js
@@ -0,0 +1,28 @@
+import { useState, useContext   , useRef } from "react";
+import ThemeContext from "../context/ThemeContext";
+
+function TaskTitle({ task }) {
+    const { tasks, setTasks } = useContext(ThemeContext);
+    const [edit, setEdit] = useState(false);
+    const [title, setTitle] = useState('');
+
+    const getTitle = val => {
+        const previousHeader = task.header;
+        const newHeader = val.target.value;
+        setTitle(newHeader.length ? newHeader : previousHeader)
+    };
+
+    const ref = useRef(null);
+
+    const changeTitle = id => {
+        setTasks(tasks.map(element => ({ ...element, header: element.id === task.id ? `${title}` : `${element.header}` })));
+        setEdit(false);
+        console.log(id)
+    }
+
+    return edit ?
+        <input className="transparent border-0 w-100" type="text" maxlength="50" ref={ref} defaultValue={`${task.header}`} onChange={getTitle} onBlur={changeTitle} onEnter={changeTitle} /> :
+        <span className="text-wrap text-break" onClick={() => setEdit(true)}>{task.header}</span>
+}
+
+export default TaskTitle
