3d65575e7ee96832813ac80479070bbac87391c1
Alex <herbalpie@gmail.com>
2022-09-19 18:48:17 +0300

    fix to name edits

--- src/components/ColumnHeader.js
+++ src/components/ColumnHeader.js
@@ -1,10 +1,21 @@
 import RemoveColumnButton from "./RemoveColumnButton";
+import { useContext, useRef } from "react";
+import ThemeContext from "../context/ThemeContext";
 
 function ColumnHeader({ column }) {
+    const { columns, setColumns } = useContext(ThemeContext);
+
+    const inputRef = useRef(null);
+
+    const saveData = id => {
+        setColumns(columns.map(column => ({...column, header: column.id === id ? inputRef.current.value : column.header})));
+    }
 
     return <div className="d-flex justify-content-between align-items-center">
-        <input className="transparent border-0 w-100" type="text" maxlength="50" id="title-input" autoComplete="off"
-        defaultValue={`${column.header}`}/><RemoveColumnButton column={column} />
+        <input className="transparent border-0 w-100" type="text" maxLength="50" autoComplete="off" ref={inputRef}
+        value={`${column.header}`}
+        onChange={() => saveData(column.id)}/>
+        <RemoveColumnButton column={column} />
         </div>
 }
 
