0461373427bea9febce483cbc6f5d67d4e775194
Alex <herbalpie@gmail.com>
2022-09-19 15:32:53 +0300

    bugfix

--- src/components/RemoveTaskButton.js
+++ src/components/RemoveTaskButton.js
@@ -8,7 +8,7 @@ function RemoveTaskButton({ task }) {
         setTasks(tasks.filter(task => !(id === task.id)).map(task => ({...task, id: task.id > id ? task.id-1 : task.id})));
     };
 
-    return <button className="border-0 transparent" onClick={() => removeTask(task.id)}><h5 className="text-white">X</h5></button>
+    return <button className="border-0 transparent" onClick={() => removeTask(task.id)}><h6 className="text-white">X</h6></button>
 }
 
 export default RemoveTaskButton
