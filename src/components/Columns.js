15d28d6caf940200c2260d6e94e119f927c6ee9f
Alex <herbalpie@gmail.com>
2022-09-22 15:28:01 +0300

    after

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,10 +1,14 @@
+import '../App.css'
 import { useContext, useEffect } from "react"
-import ThemeContext from "../context/ThemeContext"
-import Column from "./Column"
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import AddColumnButton from "./AddColumnButton"
+import Task from './Task'
+import AddTaskButton from './AddTaskButton'
+import ColumnHeader from './ColumnHeader'
+import ThemeContext from "../context/ThemeContext"
 
 function Columns() {
-    const { columns, setColumns, setTasks } = useContext(ThemeContext);
+    const { tasks, setTasks, columns, setColumns } = useContext(ThemeContext);
 
     useEffect(() => {
         setColumns([
@@ -20,11 +24,65 @@ function Columns() {
         ]);
     }, []);
 
+    function handleOnDragEnd(result, columns, setColumns) {
+        console.log(result)
+        const { source, destination } = result;
+
+        if (!result.destination) return;
+
+        if (source.droppableId !== destination.droppableId) {
+            const sourceColumn = columns[source.droppableId];
+            const destColumn = columns[destination.droppableId];
+            const sourceItems = [...sourceColumn.items];
+            const destItems = [...destColumn.items];
+            const [removed] = sourceItems.splice(source.index, 1);
+            destItems.splice(destination.index, 0, removed);
+            setColumns({
+              ...columns,
+              [source.droppableId]: {
+                ...sourceColumn,
+                items: sourceItems
+              },
+              [destination.droppableId]: {
+                ...destColumn,
+                items: destItems
+              }
+            });
+          } else {
+        const items = Array.from(tasks);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+        setTasks(items);
+        }
+    }
+
     return <div className="d-flex justify-content-start align-items-start">
-        {columns.map(column => <Column
-            key={column.id}
-            column={column}
-        />)}
+        <DragDropContext onDragEnd={result => handleOnDragEnd(result)}>
+            {columns.map(column => <div className="column-width m-4" key={column.id} >
+                <div className='yellow p-2 text-center rounded-top border border-bottom-0 border-secondary'>
+                    <h4><ColumnHeader column={column} /> {column.id}</h4>
+                </div>
+                <div className='grey py-1 px-4 rounded-bottom border border-top-0 border-secondary'>
+                    <Droppable droppableId={`${column.id}`} key={column.id}>
+                        {(provided) => (
+                            <div {...provided.droppableProps} ref={provided.innerRef}>
+                                {tasks.map((task, index) => task.colNum === column.id ?
+                                    <Draggable draggableId={`${task.id}`} key={task.id}  index={index}>
+                                        {(provided) => (
+                                            <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+                                                <Task column={column} task={task} />
+                                            </div>
+                                        )}
+                                    </Draggable> : '')
+                                }
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                    <AddTaskButton column={column} />
+                </div>
+            </div>)}
+        </DragDropContext>
         <AddColumnButton />
     </div>
 };
